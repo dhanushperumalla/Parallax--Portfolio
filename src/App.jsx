d52e52c79110bbfa13aa2568472deb5427fc2297
Perumalla Venkata Naga Dhansush <perumalladhanush102@gmail.com>
2024-03-25 21:12:09 +0530

    Parallax Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import "./app.scss";
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/navbar";
+import Parallax from "./components/parallax/Parallax";
 
 const App = () => {
   return (
     <div>
       <section id="Homepage">
         <Navbar />
+        <Hero />
       </section>
-      <section id="Services">Parallax</section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
       <section>Services</section>
-      <section id="Portfolio">Parallax</section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
